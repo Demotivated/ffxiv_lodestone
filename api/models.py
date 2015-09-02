3ee964ed57feddb4202e0ac14e6abe057c517487
demotivated <selahmadie@gmail.com>
2015-09-02 16:33:18 -0700

    Create character model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    lodestone_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    server = models.CharField(max_length=20)
+    species = models.CharField(max_length=20)
+    city_state = models.CharField(max_length=20)
+    free_company = models.CharField(max_length=100)
--- fxiv_lodestone/settings.py
+++ fxiv_lodestone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'opbeat.contrib.django',
+    'api'
 )
 
 OPBEAT = {
