556d40f2baace7e3eba7c0ac8efd0a041f04eef3
demotivated <selahmadie@gmail.com>
2015-09-04 16:20:59 -0700

    Better logging when parsing

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 import logging
 
 import requests
@@ -16,19 +16,23 @@ def parse_by_id(request, lodestone_id):
     tree = html.fromstring(page.text)
 
     char = Character()
-    char.lodestone_id = lodestone_id
-    char.name = tree.xpath('//title/text()')[0].split('|')[0].strip()
+    try:
+        char.lodestone_id = lodestone_id
+        char.name = tree.xpath('//title/text()')[0].split('|')[0].strip()
 
-    info = tree.xpath('//dl[@class="chara_profile_box_info clearfix"]//dd[@class="txt_name"]/text()')
-    _, _, char.city_state, char.grand_company = info
+        info = tree.xpath('//dl[@class="chara_profile_box_info clearfix"]//dd[@class="txt_name"]/text()')
+        _, _, char.city_state, char.grand_company = info
 
-    char.free_company = tree.xpath('//dd[@class="txt_name"]/a[contains(@href, "")]/text()')[0]
+        char.free_company = tree.xpath('//dd[@class="txt_name"]/a[contains(@href, "")]/text()')[0]
 
-    char.server = tree.xpath('//h2//span/text()')[0].strip()[1:-1]
+        char.server = tree.xpath('//h2//span/text()')[0].strip()[1:-1]
 
-    info = tree.xpath('//div[@class="chara_profile_title"]')
-    species, _, _ = info[0].text.split('/')
-    char.species = species.strip()
+        info = tree.xpath('//div[@class="chara_profile_title"]')
+        species, _, _ = info[0].text.split('/')
+        char.species = species.strip()
 
-    char.save()
+        char.save()
+    except ValueError:
+        logging.error('Unable to parse id {} from lodestone'.format(lodestone_id), exc_info=True)
+        return HttpResponseServerError()
     return HttpResponse()
