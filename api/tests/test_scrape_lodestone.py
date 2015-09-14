03d3daa0c1976fa93e83b605d2013ace8f655ebe
demotivated <selahmadie@gmail.com>
2015-09-13 23:29:21 -0700

    Add unit test for JSON response

--- api/tests/test_scrape_lodestone.py
+++ api/tests/test_scrape_lodestone.py
@@ -6,20 +6,34 @@ from api.models import Character
 
 class ScrapeFromLodestoneTestCase(TestCase):
 
-    def test_scrape_by_id(self):
-        client = Client()
-        lodestone_id = '8774791'
-
-        response = client.post(reverse('scrape_by_id', kwargs={
-            'lodestone_id': lodestone_id
+    def setUp(self):
+        self.client = Client()
+        self.lodestone_id = '8774791'
+        self.response = self.client.post(reverse('scrape_by_id', kwargs={
+            'lodestone_id': self.lodestone_id
         }))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
-        mina = Character.objects.get(lodestone_id=lodestone_id)
+    def test_inside_database(self):
+        mina = Character.objects.get(lodestone_id=self.lodestone_id)
         self.assertEqual('Mina Loriel', mina.name)
-        self.assertEqual(lodestone_id, mina.lodestone_id, )
+        self.assertEqual(self.lodestone_id, mina.lodestone_id)
         self.assertEqual('Order of the Twin Adder/Second Serpent Lieutenant', mina.grand_company)
         self.assertEqual('Gridania', mina.city_state)
         self.assertEqual('Zanarkand', mina.free_company)
         self.assertEqual('Zalera', mina.server)
         self.assertEqual('Miqo\'te', mina.species)
+
+    def test_json_response(self):
+        self.assertJSONEqual(
+            self.response.content.decode('utf-8'),
+            {
+                "name": "Mina Loriel",
+                "lodestone_id": "8774791",
+                "grand_company": "Order of the Twin Adder/Second Serpent Lieutenant",
+                "city_state": "Gridania",
+                "free_company": "Zanarkand",
+                "server": "Zalera",
+                "species": "Miqo'te",
+            }
+        )
