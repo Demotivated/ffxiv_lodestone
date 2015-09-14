c572dca47aed49ffd995a2d6076ecb52382bbc34
demotivated <selahmadie@gmail.com>
2015-09-13 23:46:07 -0700

    More specific HTML parsing

--- api/views.py
+++ api/views.py
@@ -25,13 +25,13 @@ def scrape_by_id(request, lodestone_id):
         try:
             char = Character()
             html = BeautifulSoup(page.text, 'html.parser')
-            elements = html.find_all('dd')
+            elements = html.find_all('dd', attrs={'class': 'txt_name'})
 
             char.lodestone_id = lodestone_id
             char.name = html.title.string.split('|')[0].strip()
-            char.city_state = elements[5].text
-            char.grand_company = elements[7].text
-            char.free_company = elements[9].text
+            char.city_state = elements[2].text
+            char.grand_company = elements[3].text
+            char.free_company = elements[4].text
             char.server = html.find(string=re.compile('^ \([A-Z][a-z]+\)$'))[2:-1]
             char.species = html.find('div', attrs={'class': 'chara_profile_title'}).text.split('/')[0].strip()
 
